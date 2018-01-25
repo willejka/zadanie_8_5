36e3ee7ea95c324681a36efd610614f448501cd9
Willejka <ewasinska@gmail.com>
2018-01-25 19:07:13 +0100

    Initial commit

---
+++ js/zadanie_8_5.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var a = 5,
+    h = 8,
+    triangleArea = a*h/2;
+
+confirm('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);   
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
